b378b47f110e969c54b881a959c1fca6520ee993
LanPet987 <115153511+LanPet987@users.noreply.github.com>
2025-09-25 13:52:15 +0200

    tesztelt, dokumentált program

--- panziok/index.js
+++ panziok/index.js
@@ -30,8 +30,27 @@ app.get('/', (req, res) => {
 
 // B) feladat : Szobák kihasználtsága
 
+app.get('/kihasznaltsag', (req, res) => {
+ const sql = 'SELECT foglalasok.szoba AS "Szoba", foglalasok.fo AS "Vendegek", DATEDIFF(foglalasok.tav,foglalasok.erk) AS "Éjszakák" FROM foglalasok ORDER BY foglalasok.szoba ASC';
+ db.query(sql, (err, results) => {
+    if (err) {
+        return res.status(500).json({err});
+    }
+    res.json(results);
+ });
+})
 
+// C) feladat : A választott szoba foglaltsága
 
+app.get ('/foglalt' , (req, res) => {
+    const sql = 'SELECT foglalasok.szoba AS "Szoba", vendegek.vnev AS "Vendég", foglalasok.erk AS "Érkezés", foglalasok.tav AS "Távozás" FROM foglalasok INNER JOIN vendegek ON foglalasok.vendeg = vendegek.vsorsz ORDER BY vendegek.vnev ASC';
+    db.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({err});
+        }
+        res.json(results);
+    });
+});
 
 
 
